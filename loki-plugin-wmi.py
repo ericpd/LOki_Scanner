0e14c37f8e061f5cb90c7d6ac8ff20387d79d510
ericpd <saro.seda2003@gmail.com>
2022-07-26 08:02:24 -0700

    Update loki-plugin-wmi.py

--- loki-plugin-wmi.py
+++ loki-plugin-wmi.py
@@ -74,3 +74,12 @@ LokiRegisterPlugin("PluginWMI", ScanWMI, 1)  # noqa: F821 undefined name 'LokiRe
 
 
 .
+if sys.platform in ("win32", "cygwin"):
+        try:
+            import wmi
+        except ImportError as e:
+            wmi = None
+            logger.log("CRITICAL", "WMIScan", "Unable to import wmi")
+            print("Unable to import wmi")
+        oWMI = wmi.WMI(namespace=r'root\subscription')
+
